f17b9b5ce33d0fa4a59b271ac9b6405df6e3743a
AleksandrAlex <suslovalmi@mail.ru>
2021-03-12 23:14:17 +0300

    added account window and profile window

--- app/src/main/java/ru/suslovalex/hackathonautorisation/MainActivity.kt
+++ app/src/main/java/ru/suslovalex/hackathonautorisation/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.suslovalex.hackathonautorisation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.suslovalex.hackathonautorisation.ui.AccountCreateFragment
+import ru.suslovalex.hackathonautorisation.ui.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.container_layout, AccountCreateFragment()).commit()
     }
 }
---
+++ app/src/main/java/ru/suslovalex/hackathonautorisation/ui/AccountCreateFragment.kt
@@ -0,0 +1,9 @@
+package ru.suslovalex.hackathonautorisation.ui
+
+
+import androidx.fragment.app.Fragment
+import ru.suslovalex.hackathonautorisation.R
+
+class AccountCreateFragment : Fragment(R.layout.fragment_account_create) {
+
+}
