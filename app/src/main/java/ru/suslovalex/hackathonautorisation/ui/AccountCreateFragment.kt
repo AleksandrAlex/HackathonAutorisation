95830bb614f557615f04843c9fbe5fa91eb19c43
AleksandrAlex <suslovalmi@mail.ru>
2021-03-13 15:36:05 +0300

    add profile fragment

--- app/src/main/java/ru/suslovalex/hackathonautorisation/ui/AccountCreateFragment.kt
+++ app/src/main/java/ru/suslovalex/hackathonautorisation/ui/AccountCreateFragment.kt
@@ -1,9 +1,22 @@
 package ru.suslovalex.hackathonautorisation.ui
 
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import ru.suslovalex.hackathonautorisation.R
 
 class AccountCreateFragment : Fragment(R.layout.fragment_account_create) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnCreate: Button = view.findViewById(R.id.btnCreate)
+        btnCreate.setOnClickListener {
+            it.findNavController().navigate(R.id.action_accountCreateFragment_to_profileFragment)
+        }
+    }
 
 }
--- app/src/main/java/ru/suslovalex/hackathonautorisation/ui/ConfirmAccountFragment.kt
+++ app/src/main/java/ru/suslovalex/hackathonautorisation/ui/ConfirmAccountFragment.kt
@@ -1,7 +1,20 @@
 package ru.suslovalex.hackathonautorisation.ui
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import ru.suslovalex.hackathonautorisation.R
 
 class ConfirmAccountFragment: Fragment(R.layout.fragment_confirm_account) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnConfirmAccount: Button = view.findViewById(R.id.btnOk)
+        btnConfirmAccount.setOnClickListener {
+            it.findNavController().navigate(R.id.action_confirmAccountFragment_to_profileFragment)
+        }
+    }
 }
