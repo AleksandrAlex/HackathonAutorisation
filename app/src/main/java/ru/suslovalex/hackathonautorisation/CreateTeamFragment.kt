978cdb4393c11b558d218f7b65e0e78493ab3aa2
AleksandrAlex <suslovalmi@mail.ru>
2021-03-13 21:00:38 +0300

    add fab and CreateTeamFragment.kt

---
+++ app/src/main/java/ru/suslovalex/hackathonautorisation/CreateTeamFragment.kt
@@ -0,0 +1,25 @@
+package ru.suslovalex.hackathonautorisation
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+
+
+class CreateTeamFragment : Fragment(R.layout.fragment_create_team) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnAddTeam: Button = view.findViewById(R.id.btnAddTeam)
+        btnAddTeam.setOnClickListener {
+
+            // add new team in database and recyclerview
+            it.findNavController().navigate(R.id.action_createCommandFragment_to_listAllTeamsFragment)
+        }
+    }
+
+
+
+}
--- app/src/main/java/ru/suslovalex/hackathonautorisation/ListAllTeamsFragment.kt
+++ app/src/main/java/ru/suslovalex/hackathonautorisation/ListAllTeamsFragment.kt
@@ -3,25 +3,38 @@ package ru.suslovalex.hackathonautorisation
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ru.suslovalex.hackathonautorisation.data.Team
 import ru.suslovalex.hackathonautorisation.ui.AdapterTeam
 
 
 class ListAllTeamsFragment : Fragment(R.layout.fragment_list_all_teams) {
 
+    private val teamAdapter = AdapterTeam()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val teamAdapter = AdapterTeam()
+        setupUi(view)
+        setupFab(view)
+    }
 
+    private fun setupFab(view: View) {
+        val fab: FloatingActionButton = view.findViewById(R.id.fab)
+        fab.setOnClickListener {
+            it.findNavController().navigate(R.id.action_listAllTeamsFragment_to_createCommandFragment)
+        }
+    }
+
+    private fun setupUi(view: View) {
         val teamsList: RecyclerView = view.findViewById(R.id.teams_list)
         teamsList.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = teamAdapter
         }
-
         val list = mutableListOf<Team>()
         for (i in 0..20){
             list.add(Team(name = "team $i", description = "description $i"))
