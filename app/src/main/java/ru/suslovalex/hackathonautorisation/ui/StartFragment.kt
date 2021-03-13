296b4aa5449d3e6939b78badfd2175302730864f
AleksandrAlex <suslovalmi@mail.ru>
2021-03-13 13:47:35 +0300

    navigation between StartFragment, CreateFragment, ConfirmFragment works

--- app/src/main/java/ru/suslovalex/hackathonautorisation/ui/StartFragment.kt
+++ app/src/main/java/ru/suslovalex/hackathonautorisation/ui/StartFragment.kt
@@ -1,7 +1,26 @@
 package ru.suslovalex.hackathonautorisation.ui
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import ru.suslovalex.hackathonautorisation.R
 
 class StartFragment: Fragment(R.layout.fragment_start) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnCreateAccount: Button = view.findViewById(R.id.btnCreateAccount)
+        btnCreateAccount.setOnClickListener {
+            it.findNavController().navigate(R.id.action_startFragment_to_accountCreateFragment)
+        }
+
+        val btnConfirmAccount: Button = view.findViewById(R.id.btnConfirmAccount)
+        btnConfirmAccount.setOnClickListener {
+            it.findNavController().navigate(R.id.action_startFragment_to_confirmAccountFragment)
+        }
+
+    }
 }
